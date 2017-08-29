9d1a28849c69d771f80e349fab9e1c9554c4a6d0
Bill Duncan <wdduncan@gmail.com>
2017-08-29 02:56:23 -0400

    add code to produce labels for generated entities

--- src/translate_data_source.py
+++ src/translate_data_source.py
@@ -25,7 +25,9 @@ def ttl_prefixes(tablename, base_uri="", ontology_uri="", imports=""):
                 
                 # custom prefixes
                 @base <{0}> .
-                @prefix : <http://purl.obolibrary.org/obo/db_mapping.owl/> .
+                @prefix : <{0}> .
+                @prefix dso: <http://purl.obolibrary.org/obo/data_source_ontology.owl/> .
+                @prefix data_source_ontology: <http://purl.obolibrary.org/obo/data_source_ontology.owl/> .
                 @prefix table: <table/{1}/> .
                 @prefix tablename: <table/{1}> .
                 @prefix field: <field/{1}/> .
@@ -35,8 +37,9 @@ def ttl_prefixes(tablename, base_uri="", ontology_uri="", imports=""):
                 @prefix data_property: <data_property/{1}/> .
                 @prefix dv: <data_property/{1}/> .
 
-                # ontology uri
-                <{2}> rdf:type owl:Ontology .
+                # ontology uri and default import
+                <{2}> rdf:type owl:Ontology;
+                      owl:imports <http://purl.obolibrary.org/obo/data_source_ontology.owl> .
                 """.format(base_uri, tablename, ontology_uri))
 
     if len(imports) > 0:
@@ -44,29 +47,31 @@ def ttl_prefixes(tablename, base_uri="", ontology_uri="", imports=""):
         if type(imports) == type(""): # imports is a list
             ttl += \
                 dedent("""
-                        # ontology imports
+                        # specified ontology imports
                         <{0}> owl:imports <{1}> .""".format(ontology_uri, imports))
         elif type(imports) == type([]): # imports is a list
             # use list comprehension ["<" + i + ">" for i in imports] to build list of uris
             # then join with commas
             ttl += \
                 dedent("""
-                        # ontology imports
+                        # specified ontology imports
                         <{0}> owl:imports {1} .""".format(ontology_uri,
                                                           ", ".join(["<" + i + ">" for i in imports])))
 
     return ttl
 
 # @print_function_output()
-def ttl_table(table_uri):
+def ttl_table(table_uri, tablename):
     ttls = ["\n# axioms to create table"]
     
     # create table class
-    ttl = declare_class("tablename:", ":table")
+    label = "{0} table".format(tablename)
+    ttl = declare_class("tablename:", "dso:table", label)
     ttls.append(ttl)
 
     #  create instance of table
-    ttl = declare_instance(table_uri, "tablename:")
+    label = "{0} table instance".format(tablename)
+    ttl = declare_instance(table_uri, "tablename:", label=label)
     ttls.append(ttl)
 
     # join all ttl statements
@@ -74,14 +79,14 @@ def ttl_table(table_uri):
     return ttl
 
 # @print_function_output()
-def ttl_fields(table_uri, fields):
+def ttl_fields(table_uri, tablename, fields):
     ttls = ["\n# axioms to create fields"]
 
     for field_name in fields:
-        ttl = ttl_field(table_uri, field_name) # create ttl for that field
+        ttl = ttl_field(table_uri, tablename, field_name) # create ttl for that field
         ttls.append(ttl)
 
-        ttl = ttl_field_data_property(field_name) # create data property field for that field
+        ttl = ttl_field_data_property(tablename, field_name) # create data property field for that field
         ttls.append(ttl + "\n") # add new line to help visual inspection
 
     # join all ttl statements
@@ -89,17 +94,19 @@ def ttl_fields(table_uri, fields):
     return ttl
 
 # @print_function_output()
-def ttl_field(table_uri, field_name):
+def ttl_field(table_uri, tablename, field_name):
     ttls = []
 
     # create field class
     class_uri = "field:{0}".format(field_name)
-    ttl = declare_class(class_uri, ":field")
+    label = "{0}.{1} field".format(tablename, field_name)
+    ttl = declare_class(class_uri, "dso:field", label)
     ttls.append(ttl)
 
     # create instance of field class
     field_uri = get_field_uri(field_name)  # uri for instance of field
-    ttl = declare_instance(field_uri, class_uri, table_uri)
+    label = "{0}.{1} field intance".format(tablename, field_name)
+    ttl = declare_instance(field_uri, class_uri, table_uri, label)
     ttls.append(ttl)
 
     # join all ttl statements
@@ -107,32 +114,34 @@ def ttl_field(table_uri, field_name):
     return ttl
 
 # @print_function_output()
-def ttl_field_data_property(field_name):
+def ttl_field_data_property(tablename, field_name):
     # create data property field
     data_prop_uri = get_data_prop_uri(field_name)
-    ttl = declare_data_property(data_prop_uri)
+    label = "{0}.{1} data value".format(tablename, field_name)
+    ttl = declare_data_property(data_prop_uri, label)
 
     return ttl
 
 # @print_function_output()
 def ttl_records(df, table_uri, tablename):
     ttls = []
-    ttls.append('\n# axioms to create records and values')
+    ttls.append("\n# axioms to create record class")
 
     # create record class for this table
     record_class_uri = "record:{0}_record".format(tablename)
-    ttl = declare_class(record_class_uri, ":record")
+    label = "{0} record".format(tablename)
+    ttl = declare_class(record_class_uri, "dso:record", label)
     ttls.append(ttl + "\n")  # add new line to help visual inspection
 
     # create field value class and subclasses
     field_names = list(df.columns)  # get list of fields
-    ttl = ttl_field_value_classes(tablename, field_names)
-    ttls.append(ttl + "\n")  # add new line to help visual inspection
-
     for record_idx, record in enumerate(df.itertuples(), 1):
+        ttls.append("\n# axioms to create record")
+
         # create instance of record
         record_uri = get_record_uri(tablename, record_idx)
-        ttl = declare_instance(record_uri, record_class_uri, table_uri)
+        label = "{0} record {1}".format(tablename, str(record_idx))
+        ttl = declare_instance(record_uri, record_class_uri, table_uri, label)
         ttls.append(ttl)
 
         ttl = ttl_field_values(tablename, record, record_idx, record_uri, field_names)
@@ -149,64 +158,103 @@ def ttl_field_value_classes(tablename, field_names):
 
     # create field value class for this table
     class_uri = get_field_value_class_uri(tablename)
-    ttl = declare_class(class_uri, ":field_value")
-    ttls.append(ttl)
+    label = "{0} field value".format(tablename)
+    ttl = declare_class(class_uri, "dso:field_value", label)
+    ttls.append(ttl + "\n") # add new line to help visual inspection
 
     # for every field create a field value value subclass
     for field_name in field_names:
         fv_class_uri = get_field_value_class_uri(tablename, field_name)
-        ttl = declare_class(fv_class_uri, class_uri)
-        ttls.append(ttl)  # add new line to help visual inspection
-
-    # join all ttl statements
-    ttl = "\n".join(ttls)
-    return ttl
-
-#  @print_function_output()
-def ttl_field_values(tablename, record, record_idx, record_uri, field_names):
-    ttls = []
-
-    for value_idx, value in enumerate(record):
-        if value_idx > 0:
-            field_name = field_names[value_idx - 1]
-            field_uri = get_field_uri(field_name)  # uri for instance of field
-            data_prop_uri = get_data_prop_uri(field_name) # uri for field as data property
-
-            ttl = ttl_field_value(tablename, record_idx, value, record_uri, field_uri, field_name, data_prop_uri)
-            ttls.append(ttl)
+        label = "{0}.{1} field value".format(tablename, field_name)
+        ttl = declare_class(fv_class_uri, class_uri, label)
+        ttls.append(ttl + "\n")  # add new line to help visual inspection
 
     # join all ttl statements
     ttl = "\n".join(ttls)
     return ttl
 
 # @print_function_output()
-def ttl_field_value(tablename, record_idx, value, record_uri, field_uri, field_name, data_prop_uri):
-    ttls = []
+def ttl_field_values(tablename, record, record_idx, record_uri, field_names):
+    ttls = ["\n# axioms to create field values"]
 
-    # create uri for field value (fv) instance and class
-    fv_uri = get_field_value_uri(field_name, record_idx)
-    fv_class_uri = get_field_value_class_uri(tablename, field_name)
+    for value_idx, value in enumerate(record):
+        # value_idx 0 holds the index of the value tuple, so ignore it
+        if value_idx > 0:
+            field_name = field_names[value_idx - 1]
+            field_uri = get_field_uri(field_name)  # uri for instance of field
+            data_prop_uri = get_data_prop_uri(field_name) # uri for field as data property
 
-    # create instance
-    ttl = "{0} rdf:type {1} .".format(fv_uri, fv_class_uri)
-    ttls.append(ttl)
+            # relate record to data value
+            ttl = ttl_record_data_value(record_uri, data_prop_uri, value)
+            ttls.append(ttl)
 
-    # relate fv to record and field
-    ttl = "{0} :member_of {1}, {2} .".format(fv_uri, record_uri, field_uri)
-    ttls.append(ttl)
+            # create field value (fv) instance
+            fv_uri = get_field_value_uri(field_name, record_idx)
+            label = "{0}.{1} field value {2}".format(tablename, field_name, str(record_idx))
+            fv_class_uri = get_field_value_class_uri(tablename, field_name)
+            ttl = declare_instance(fv_uri, fv_class_uri, label=label)
+            ttls.append(ttl)
 
-    # relate fv to value
-    ttl = """{0} :has_data_value "{1}" .""".format(fv_uri, value)
-    ttls.append(ttl)
+            # fv is member of field and record
+            ttl = ttl_field_value_membership(fv_uri, record_uri, field_uri)
+            ttls.append(ttl)
 
-    # relate record to value
-    ttl = """{0} {1} "{2}" .""".format(record_uri, data_prop_uri, value)
-    ttls.append(ttl)
+            # relate fv to datum
+            ttl = ttl_field_value_datum(fv_uri, value)
+            ttls.append(ttl)
+
+            # ttl = ttl_field_value(tablename, record_idx, value, record_uri, field_uri, field_name, data_prop_uri)
+            # ttls.append(ttl)
+
 
     # join all ttl statements
     ttl = "\n".join(ttls)
     return ttl
 
+# # @print_function_output()
+# def ttl_field_value(tablename, record_idx, value, record_uri, field_uri, field_name, data_prop_uri):
+#     ttls = []
+#
+#     # create uri for field value (fv) instance and class
+#     fv_uri = get_field_value_uri(field_name, record_idx)
+#     fv_class_uri = get_field_value_class_uri(tablename, field_name)
+#
+#     # create instance
+#     ttl = "{0} rdf:type {1} .".format(fv_uri, fv_class_uri)
+#     ttls.append(ttl)
+#
+#     # relate fv to record and field
+#     ttl = "{0} :member_of {1}, {2} .".format(fv_uri, record_uri, field_uri)
+#     ttls.append(ttl)
+#
+#     # relate fv to value
+#     ttl = """{0} dso:has_data_value "{1}" .""".format(fv_uri, value)
+#     ttls.append(ttl)
+#
+#     # relate record to data value
+#     ttl = """{0} {1} "{2}" .""".format(record_uri, data_prop_uri, value)
+#     ttls.append(ttl)
+#
+#     # join all ttl statements
+#     ttl = "\n".join(ttls)
+#     return ttl
+
+# @print_function_output()
+def ttl_field_value_membership(field_value_uri, record_uri, field_uri):
+    ttl = "{0} dso:member_of {1}, {2} .".format(field_value_uri, record_uri, field_uri)
+    return ttl
+
+# @print_function_output()
+def ttl_field_value_datum(field_value_uri, value):
+    # relate field value to a datum value
+    ttl = """{0} dso:has_data_value "{1}" .""".format(field_value_uri, value)
+    return ttl
+
+# @print_function_output()
+def ttl_record_data_value(record_uri, data_prop_uri, value):
+    # relate record to data value
+    ttl = """{0} {1} "{2}" .""".format(record_uri, data_prop_uri, value)
+    return ttl
 
 def translate_data_to_ttl(filepath, base_uri="", ontology_uri="", imports=""):
     # ceate dataframe from demo data
@@ -214,32 +262,23 @@ def translate_data_to_ttl(filepath, base_uri="", ontology_uri="", imports=""):
 
     # just get table and file name part of path
     tablename = get_tablename_from_file(filepath)
+    table_uri = get_table_uri()
     filename = get_tablename_from_file(filepath, remove_ext=False)
+    field_names = list(df.columns)  # get list of fields
 
     # specify ontology variables
     if len(base_uri) == 0:
-        base_uri = "http://purl.obolibrary.org/obo/db_mapping.owl/{0}/".format(tablename)
+        base_uri = "http://purl.obolibrary.org/obo/data_source_ontology.owl/{0}/".format(tablename)
     if len(ontology_uri) == 0:
-        ontology_uri = "http://purl.obolibrary.org/obo/db_mapping.owl/{0}".format(filename)
-    if len(imports) == 0:
-        imports = "http://purl.obolibrary.org/obo/db_mapping.owl"
+        ontology_uri = "http://purl.obolibrary.org/obo/data_source_ontology.owl/{0}".format(filename)
 
-    # list to hold axioms
-    axioms = []
-
-    # add prefixes
-    axioms.append(ttl_prefixes(tablename, base_uri, ontology_uri, imports=imports))
-
-    # add table
-    table_uri = get_table_uri()
-    axioms.append(ttl_table(table_uri))
-
-    # add fields
-    fields = list(df.columns) # get list of fields
-    axioms.append(ttl_fields(table_uri, fields))
-
-    # add records and values
-    axioms.append(ttl_records(df, table_uri, tablename))
+    # add axioms to represent table, fields, field values, records
+    axioms = [] # list to hold axioms
+    axioms.append(ttl_prefixes(tablename, base_uri, ontology_uri, imports=imports)) # add prefixes
+    axioms.append(ttl_table(table_uri, tablename)) # add table
+    axioms.append(ttl_fields(table_uri, tablename, field_names)) # add fields
+    axioms.append(ttl_field_value_classes(tablename, field_names)) # add field value classes
+    axioms.append(ttl_records(df, table_uri, tablename)) # add records and values
 
     return axioms
 
--- src/uri_functions.py
+++ src/uri_functions.py
@@ -4,55 +4,52 @@ def get_uri(val, base=""):
     else:
         return "<" + val + ">"
 
-def get_table_uri(tablename='', base=""):
-    if len(tablename) > 0:
-        return get_uri(tablename + "_table_i", base)
+def get_uri_label(uri, label):
+    if len(label) > 0:
+        return """\n{0} rdfs:label "{1}" .""".format(uri, label)
     else:
-        return "tablename:_table_i"
+        return ""
 
-def get_field_uri(field_name, tablename='', base=""):
-    if len(tablename) > 0:
-        return get_uri(tablename + "/field/" + field_name + "_field_i", base)
-    else:
-        return "field:" + field_name + "_field_i"
+def get_table_uri(tablename="", base=""):
+    return "tablename:_table_i"
 
-def get_data_prop_uri(field_name, tablename='', base=""):
-    if len(tablename) > 0:
-        return get_uri(tablename + "/data_property/" + field_name + "_data_value", base)
-    else:
-        return "data_property:" + field_name + "_data_value"
+def get_field_uri(field_name, tablename="", base=""):
+    return "field:" + field_name + "_field_i"
+
+def get_data_prop_uri(field_name, tablename="", base=""):
+    return "data_property:" + field_name + "_data_value"
 
 def get_record_uri(tablename, record_idx, record_name='', base=""):
-    if len(record_name) > 0:
-        return get_uri(tablename + "/record/" + record_name + str(record_idx) + "_i", base)
-    else:
-        return "record:" + tablename + "_record_" + str(record_idx) + "_i"
+    return "record:" + tablename + "_record_" + str(record_idx) + "_i"
 
 def get_field_value_uri(field_name, record_idx, tablename='', base=""):
-    if len(tablename) > 0:
-        return get_uri(tablename + "/field_value/" + field_name + "_field_value_" + str(record_idx) + "_i", base)
-    else:
-        return "field_value:" + field_name + "_field_value_" + str(record_idx) + "_i"
+    return "field_value:" + field_name + "_field_value_" + str(record_idx) + "_i"
 
 def get_field_value_class_uri(tablename, field_name=""):
     if len(field_name) > 0:
-        return "field_value:{0}_{1}_field_value".format(tablename, field_name)
+        return "field_value:{0}.{1}_field_value".format(tablename, field_name)
     else:
         return "field_value:{0}_field_value".format(tablename)
 
-
-def declare_instance(insance_uri ,class_uri, member_uri=""):
+def declare_instance(insance_uri ,class_uri, member_uri="", label=""):
+    ttl = "{0} rdf:type owl:NamedIndividual, {1} .".format(insance_uri, class_uri)
     if len(member_uri) > 0:
-        return "{0} rdf:type owl:NamedIndividual, {1}; :member_of {2} .".format(insance_uri, class_uri, member_uri)
-    else:
-        return "{0} rdf:type {1} .".format(insance_uri, class_uri)
-    pass
+        ttl += "\n{0} dso:member_of {1} .".format(insance_uri, member_uri)
+    if len(label) > 0:
+        ttl += get_uri_label(insance_uri, label)
+    return ttl
 
-def declare_class(class_uri, super_uri=""):
+def declare_class(class_uri, super_uri="", label=""):
+    ttl = ttl = "{0} rdf:type owl:Class .".format(class_uri)
     if len(super_uri) > 0:
-        return "{0} rdf:type owl:Class; rdfs:subClassOf {1} .".format(class_uri, super_uri)
-    else:
-        return "{0} rdf:type owl:Class .".format(class_uri)
+        ttl += "\n{0} rdfs:subClassOf {1} .".format(class_uri, super_uri)
+    if len(label) > 0:
+        ttl += get_uri_label(class_uri, label)
+    return ttl
 
-def declare_data_property(data_property_uri):
-    return "{0} rdf:type owl:DatatypeProperty .".format(data_property_uri)
+def declare_data_property(data_property_uri, label=""):
+    ttl = "{0} rdf:type owl:DatatypeProperty .".format(data_property_uri)
+    if len(label) > 0:
+        ttl += get_uri_label(data_property_uri, label)
+    return ttl
+
