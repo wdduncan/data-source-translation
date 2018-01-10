193531c5a971f6df554d251011805025325c9c15
Bill Duncan <wdduncan@gmail.com>
2018-01-10 00:24:25 -0500

    add ttl translation for services

--- simple_dental_demo/lib/ttl_template.py
+++ simple_dental_demo/lib/ttl_template.py
@@ -33,76 +33,80 @@ def ttl_prefixes(base="", ontology_uri=""):
     return ttl
 
 
-def declare_individual(uri, class_uri, label=""):
+def ttl_declare_individual(uri, class_uri, label=""):
     ttl = dedent("""{0} rdf:type owl:NamedInvidual, {1} .\n""".format(uri, class_uri))
     if len(label.strip()) > 0:
-        ttl += """{0} rdfs:label {1} .""".format(uri, label)
+        ttl += """{0} rdfs:label "{1}" .\n""".format(uri, label)
     return ttl
 
 
-def declare_class(class_uri, label=""):
+def ttl_declare_class(class_uri, label=""):
     ttl = """{0} rdf:type owl:Class .\n""".format(class_uri)
     if len(label.strip()) > 0:
-        ttl += """{0} rdfs:label {1} .""".format(class_uri, label)
+        ttl += """{0} rdfs:label "{1}" .\n""".format(class_uri, label)
 
     return ttl
 
 
 def ttl_triple(subj, pred, obj):
-    return """{0} {1} {2} .""".format(subj, pred, obj)
+    return """{0} {1} {2} .\n""".format(subj, pred, obj)
 
 
-def part_of(uri1, uri2):
+def ttl_part_of(uri1, uri2):
     return ttl_triple(uri1, ":part_of", uri2)
 
 
-def has_part(uri1, uri2):
+def ttl_has_part(uri1, uri2):
     return ttl_triple(uri1, ":has_part", uri2)
 
 
-def has_quality(uri1, uri2):
+def ttl_has_quality(uri1, uri2):
     return ttl_triple(uri1, ":has_quality", uri2)
 
 
-def quality_of(uri1, uri2):
+def ttl_quality_of(uri1, uri2):
     return ttl_triple(uri1, ":quality_of", uri2)
 
 
-def has_participant(uri1, uri2):
+def ttl_has_participant(uri1, uri2):
     return ttl_triple(uri1, ":has_participant", uri2)
 
 
-def participates_in(uri1, uri2):
+def ttl_participates_in(uri1, uri2):
     return ttl_triple(uri1, ":participates_in", uri2)
 
 
-def birth_date(patient_uri, dob):
+def ttl_is_about(uri1, uri2):
+    return ttl_triple(uri1, ":is_about", uri2)
+
+
+def ttl_birth_date(patient_uri, dob):
     dob = """"{0}"^^xsd:date""".format(str(dob))
     return ttl_triple(patient_uri, ":birth_date", dob)
 
 
-def service_date(patient_uri, srv_date):
+def ttl_service_date(procedure_uri, srv_date):
     srv_date = """"{0}"^^xsd:date""".format(str(srv_date))
-    return ttl_triple(patient_uri, ":birth_date", srv_date)
+    return ttl_triple(procedure_uri, ":service_date", srv_date)
 
 
-def patient_uri(patient_id):
+def ttl_patient_uri(patient_id):
     return """<patient/{0}>""".format(str(patient_id))
 
 
-def provider_uri(provider_id):
+def ttl_provider_uri(provider_id):
     return """<provider/{0}>""".format(str(provider_id))
 
 
-def procedure_uri(service_id):
+def ttl_procedure_uri(service_id):
     return """<procedure/{0}>""".format(str(service_id))
 
 
-def material_uri(service_code, service_id):
+def ttl_material_uri(service_code, service_id):
     return """<material/{0}/{1}>""".format(service_code, str(service_id))
 
 
-def material_class_uri(service_code):
+def ttl_material_class_uri(service_code):
     service_code = service_code.lower()
 
     # amalgam procedures: d2140, d2150, d2160, d2161
@@ -114,23 +118,41 @@ def material_class_uri(service_code):
     else:
         return ":restoration_material"
 
-def service_code_uri(service_id, service_code):
-    ttl = """<procedure/{0}/service_code/{1}>""".format(str(service_id), service_code)
+
+def ttl_material_name(service_code):
+    service_code = service_code.lower()
+
+    # amalgam procedures: d2140, d2150, d2160, d2161
+    if service_code[0:3] == "d21":
+        return "amalagam"
+    #resin procedures: d2330, d2331, d2332, d2335
+    elif service_code[0:3] == "d23":
+        return "resin"
+    else:
+        return "restoration_material"
 
 
-def tooth_uri(patient_id, tooth):
+def ttl_service_code_uri(service_id, service_code):
+    return """<procedure/{0}/service_code/{1}>""".format(str(service_id), service_code)
+
+
+def ttl_service_code_class_uri(service_code):
+    return """:{0}""".format(service_code.upper())
+
+
+def ttl_tooth_uri(patient_id, tooth):
     return """<patient/{0}/tooth/{1}>""".format(str(patient_id), str(tooth))
 
 
-def tooth_class_uri(tooth):
+def ttl_tooth_class_uri(tooth):
     return """:tooth_{0}""".format(str(tooth))
 
 
-def surface_uri(patient_id, tooth, surface):
+def ttl_surface_uri(patient_id, tooth, surface):
     return """<patient/{0}/tooth/{1}/surface/{2}>""".format(str(patient_id), str(tooth), str(surface))
 
 
-def surface_class_uri(surface):
+def ttl_surface_class_uri(surface):
     if surface.lower() == "m":
         return ":mesial"
     elif surface.lower() == "o":
@@ -146,14 +168,33 @@ def surface_class_uri(surface):
     elif surface.lower() == "i":
         return ":incisal"
     else:
-        return ":surface"
+        return ":tooth_surface"
 
 
-def gender_uri(patient_id, gender):
+def ttl_surface_name(surface):
+    if surface.lower() == "m":
+        return "mesial surface"
+    elif surface.lower() == "o":
+        return "occlusal surface"
+    elif surface.lower() == "d":
+        return "distal surface"
+    elif surface.lower() == "f":
+        return "facial surface"
+    elif surface.lower() == "l":
+        return "lingual surface"
+    elif surface.lower() == "b":
+        return "buccal surface"
+    elif surface.lower() == "i":
+        return "incisal surface"
+    else:
+        return "tooth surface"
+
+
+def ttl_gender_uri(patient_id, gender):
     return """<patient/{0}/gender/{1}>""".format(patient_id, gender)
 
 
-def gender_class_uri(gender):
+def ttl_gender_class_uri(gender):
     if gender.lower() == "m" or gender.lower() == "male":
         return ":male_gender"
     elif gender.lower() == "f" or gender.lower() == "female":
--- simple_dental_demo/translate.py
+++ simple_dental_demo/translate.py
@@ -1,75 +1,169 @@
 import pandas as pds
 from lib.ttl_template import *
 
-def translate_patients(data_file='lib/patients_1.xlsx', output_file='output/patients_1.ttl'):
+
+def make_patient_df(data_file):
     # load Excel file into dataframe
     df = pds.ExcelFile(data_file).parse()
 
     # add column of patient uris
-    temp = map(patient_uri, df['patient_id'])
+    temp = map(ttl_patient_uri, df['patient_id'])
     df['patient_uri'] = temp
 
     # add column of gender uris
-    temp = map(gender_uri, df['patient_id'], df['gender'])
+    temp = map(ttl_gender_uri, df['patient_id'], df['gender'])
     df['gender_uri'] = temp
 
     # add column of gender class uris
-    temp = map(gender_class_uri, df['gender'])
+    temp = map(ttl_gender_class_uri, df['gender'])
     df['gender_class_uri'] = temp
 
-    slice = df[['patient_id', 'patient_uri', 'gender_uri', 'gender_class_uri', 'birth_date']]
-    for (idx, patient_id, puri, guri, gclass, dob) in slice.itertuples():
-        # create patient triples
-        label = """ "patient {0}" """.format(str(patient_id))
-        print declare_individual(puri, ":patient", label)
-        print birth_date(puri, dob) + "\n" # patient's birth date
-
-        # create gender triples
-        label = """ "patient {0} gender" """.format(str(patient_id))
-        print declare_individual(guri, gclass, label)
-        print has_quality(puri, guri) + "\n" # relate gender to patient
+    return df
 
 
+def translate_patients(data_file='lib/patients_1.xlsx',
+                       output_file='output/patients_1.ttl',
+                       print_ttl=True,
+                       save_ttl=False):
+    # create patient dataframe with uris
+    df = make_patient_df(data_file)
 
-def translate_services(data_file='lib/services_1.xlsx', output_file='output/services_1.ttl'):
+    with open(output_file, 'w') as f:
+        # local function for printing and saving turtle output
+        def output(value_str, print_ttl=print_ttl, save_ttl=save_ttl):
+            if print_ttl == True: print value_str
+            if save_ttl == True: f.write(value_str)
+
+        slice = df[['patient_id', 'patient_uri', 'gender_uri', 'gender_class_uri', 'birth_date']]
+        for (idx, patient_id, puri, guri, gclass, dob) in slice.itertuples():
+            # create patient triples
+            label = """ "patient {0}" """.format(str(patient_id))
+            output(ttl_declare_individual(puri, ":patient", label))
+            output(ttl_birth_date(puri, dob) + "\n") # patient's birth date
+
+            # create gender triples
+            label = """ "patient {0} gender" """.format(str(patient_id))
+            output(ttl_declare_individual(guri, gclass, label))
+            output(ttl_has_quality(puri, guri) + "\n") # relate gender to patient
+
+
+def make_services_df(data_file):
     # load Excel file into dataframe
     df = pds.ExcelFile(data_file).parse()
 
     # add column of patient uris
-    temp = map(patient_uri, df['patient_id'])
+    temp = map(ttl_patient_uri, df['patient_id'])
     df['patient_uri'] = temp
 
     # add column of provider uris
-    temp = map(provider_uri, df['provider_id'])
+    temp = map(ttl_provider_uri, df['provider_id'])
     df['provider_uri'] = temp
 
     # add column of tooth uris
-    temp = map(tooth_uri, df['patient_id'], df['tooth'])
+    temp = map(ttl_tooth_uri, df['patient_id'], df['tooth'])
     df['tooth_uri'] = temp
 
     # add column of tooth class uris
-    temp = map(tooth_class_uri, df['tooth'])
+    temp = map(ttl_tooth_class_uri, df['tooth'])
     df['tooth_class_uri'] = temp
 
     # add column of procedure uris
-    temp = map(procedure_uri, df['service_id'])
+    temp = map(ttl_procedure_uri, df['service_id'])
     df['procedure_uri'] = temp
 
+    # add column of service code uris
+    temp = map(ttl_service_code_uri, df['service_id'], df['service_code'])
+    df['service_code_uri'] = temp
+
+    # add column of service code class uris
+    temp = map(ttl_service_code_class_uri, df['service_code'])
+    df['service_code_class_uri'] = temp
+
     # add column of material uris
-    temp = map(material_uri, df['service_code'], df['service_id'])
+    temp = map(ttl_material_uri, df['service_code'], df['service_id'])
     df['material_uri'] = temp
 
-    #add column of material class uris
-    temp = map(material_class_uri, df['service_code'])
+    # add column of material class uris
+    temp = map(ttl_material_class_uri, df['service_code'])
     df['material_class_uri'] = temp
 
-    slice = df[['patient_id','provider_id', 'tooth', 'service_code', 'service_date',
-                'patient_uri', 'provider_uri','tooth_uri', 'tooth_class_uri',
-                'procedure_uri', 'material_uri', 'material_class_uri']]
-    for (idx, pt_id, prov_id, tooth, code, date, pt_uri, prov_uri,
-         tth_uri, tth_class_uri, proc_uri, mat_uri, mat_class_uri) in slice.itertuples():
-        print pt_uri
+    # add column of material names
+    temp = map(ttl_material_name, df['service_code'])
+    df['material_name'] = temp
 
-    print df
+    return df
 
-translate_services()
+
+def translate_services(data_file='lib/services_1.xlsx',
+                       output_file='output/services_1.ttl',
+                       print_ttl=True,
+                       save_ttl=False):
+    # create services dataframe with uris
+    df = make_services_df(data_file)
+
+    with open(output_file, 'w') as f:
+        # local function for printing and saving turtle output
+        def output(value_str, print_ttl=print_ttl, save_ttl=save_ttl):
+            if print_ttl == True: print value_str
+            if save_ttl == True: f.write(value_str)
+
+        slice = \
+            df[['patient_id','provider_id', 'tooth',
+                'surface', 'service_code', 'service_date',
+                'patient_uri', 'provider_uri','tooth_uri',
+                'tooth_class_uri','service_code_uri', 'service_code_class_uri',
+                'procedure_uri','material_uri', 'material_class_uri', 'material_name']]
+
+        for (idx, pt_id, prov_id, tooth,
+             surface, code, srv_date,
+             pt_uri, prov_uri, tth_uri,
+             tth_class_uri, code_uri, code_class_uri,
+             proc_uri, mat_uri, mat_class_uri, mat_name) in slice.itertuples():
+
+            # create triples for individuals
+            output(ttl_declare_individual(prov_uri, ":provider", "provider " + str(prov_id)))
+            output(ttl_declare_individual(proc_uri, ":procedure", "provider " + str(prov_id)))
+            output(ttl_declare_individual(code_uri, code_class_uri, code))
+            output(ttl_declare_individual
+                   (tth_uri, tth_class_uri,
+                    "tooth " + str(tooth) + " of patient " + str(pt_id)))
+
+            # since the surface field contains multiple value (e.g., "mod") loop over each surface letter
+            for s in list(surface):
+                surface_uri = ttl_surface_uri(pt_id, tooth, s)
+                surface_class_uri = ttl_surface_class_uri(s)
+                surface_name = ttl_surface_name(s)
+                output(ttl_declare_individual
+                            (surface_uri, surface_class_uri,
+                             surface_name + " of tooth " + str(tooth) + " in patient " + str(pt_id)))
+
+            output(ttl_declare_individual
+                   (mat_uri, mat_class_uri,
+                    mat_name + " placed in tooth " + str(tooth) + " of patient " + str(pt_id)) + "\n")
+
+
+            # relate individuals/data properteis
+
+            # date of service
+            output(ttl_service_date(proc_uri, srv_date))
+
+            # procedure participants
+            output(ttl_has_participant(proc_uri, pt_uri))
+            output(ttl_has_participant(proc_uri, prov_uri))
+            output(ttl_has_participant(proc_uri, tth_uri))
+            output(ttl_has_participant(proc_uri, mat_uri) + "\n")
+
+            # parthood relations
+            for s in list(surface):
+                surface_uri = ttl_surface_uri(pt_id, tooth, s)
+                output(ttl_part_of(mat_uri, surface_uri)) # material part of surface
+                output(ttl_part_of(surface_uri, tth_uri)) # surface part of tooth
+            output(ttl_part_of(tth_uri, pt_uri) + "\n") # tooth part of patient
+
+            # service code about procedure
+            output(ttl_is_about(code_uri, proc_uri) + "\n")
+
+    return df
+
+# translate_patients()
+translate_services(save_ttl=True)
